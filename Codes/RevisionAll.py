f8d52f741c78a5ea111fe80d3f2f8d67823a4acf
Tanishq chandole <tanishqchandole@gmail.com>
2026-02-15 20:04:53 +0530

    Codes

---
+++ Codes/RevisionAll.py
@@ -0,0 +1,12 @@
+print("-"*40)
+
+arr = {10,20,30,40,10}
+print(arr)
+print(type(arr))
+
+print("-"*40)
+
+book = {"Name" : "Let us C","Prize" : 350,"Author" : "Y Kanetkar"}
+print(book)
+print(type(book))
+print(book["Prize"])
