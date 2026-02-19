8d15b3fd3d84b496116ef05f3250ba0c87a45cf0
Tanishq chandole <tanishqchandole@gmail.com>
2026-02-19 22:48:39 +0530

    Codes

---
+++ Codes/FunctionDemo2.py
@@ -0,0 +1,9 @@
+# Empty Function
+
+def main():
+    pass         
+    
+
+# Starter
+if __name__ == "__main__":
+    main()
---
+++ Codes/FunctionDemo3.py
@@ -0,0 +1,10 @@
+# Not a Good Programming Practice
+
+def main():
+    print("Inside main")
+    pass
+    
+
+# Starter
+if __name__ == "__main__":
+    main()
