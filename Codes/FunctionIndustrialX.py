07d7b93f5459e1ec4b5a050108e4e66d832db0b2
Tanishq chandole <tanishqchandole@gmail.com>
2026-02-28 22:14:43 +0530

    Codes

---
+++ Codes/FunctionIndustrialX.py
@@ -0,0 +1,24 @@
+# Procedural
+
+def CheckEven(No):
+    if(No % 2 == 0):
+        return True
+    else:
+        return False  
+
+def main():
+    Value = 0
+    Ret = False
+
+    print("Enter Number :")
+    Value = int(input())
+
+    Ret = CheckEven(Value)
+
+    if(Ret == True):
+        print("Number is Even")
+    else:
+        print("Number is Odd")       
+
+if __name__ == "__main__":
+    main()    
