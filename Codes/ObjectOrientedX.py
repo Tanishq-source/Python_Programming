c3317a51f8effa507882f2cf5229eb29f934ddc4
Tanishq chandole <tanishqchandole@gmail.com>
2026-03-19 23:15:26 +0530

    Codes

---
+++ Codes/ObjectOrientedX.py
@@ -0,0 +1,21 @@
+class Arithmetic:
+    def Addition(self,A,B):
+        return A + B
+
+    def Substraction(self,A,B):
+        return A - B
+
+No1 = 0
+No2 = 0
+Ans = 0
+
+No1 = int(input("Enter first number :"))
+No2 = int(input("Enter second number :"))
+
+obj = Arithmetic()
+
+Ans = obj.Addition(No1,No2)
+print("Addition is :",Ans)
+
+Ans = obj.Substraction(No1,No2)
+print("Substraction is :",Ans)
