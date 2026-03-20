74476b04bf629d4f5f260dece666286ab213c0d0
Tanishq chandole <tanishqchandole@gmail.com>
2026-03-20 21:38:22 +0530

    Codes

---
+++ Codes/ConstructorDestructor.py
@@ -0,0 +1,16 @@
+import gc
+
+class Demo:
+    def __init__(self):
+        print("Inside Constructor")
+        
+    def __del__(self):
+        print("Inside Destructor")
+
+obj = Demo()
+
+del obj
+
+gc.collect()
+
+print("End of Application")
