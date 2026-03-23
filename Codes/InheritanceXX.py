9dd0a2171f2cc2b36af434540255b5a609b23758
Tanishq chandole <tanishqchandole@gmail.com>
2026-03-23 23:49:04 +0530

    Codes

---
+++ Codes/InheritanceXX.py
@@ -0,0 +1,21 @@
+class Parent:
+    def __init__(self):
+        print("Inside Parent Constructor")
+        
+
+    def fun(self):
+        print("Inside fun method of parent")
+
+class Child(Parent):
+    def __init__(self):
+        super().__init__()
+        print("Inside Child Constructor")
+        
+
+    def fun(self):
+        super().fun()
+        print("Inside fun method of Child")    
+
+cobj = Child()
+
+cobj.fun()
