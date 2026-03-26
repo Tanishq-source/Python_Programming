affc383d8ccf08f12043391e0bea762f7314dfac
Tanishq chandole <tanishqchandole@gmail.com>
2026-03-26 21:04:48 +0530

    Codes

---
+++ Codes/FileIOExceptionWriteX.py
@@ -0,0 +1,18 @@
+def main():
+    try:
+        fobj = open("Hello.txt","w")
+        print("File gets successfully opened")
+
+        fobj.write("Jay Ganesh Marvellous...")
+
+        fobj.close()
+
+    except FileNotFoundError:
+        print("Unable to open file as their is no such file")
+
+    finally:
+        print("End of application") 
+            
+
+if __name__ == "__main__":
+    main()
