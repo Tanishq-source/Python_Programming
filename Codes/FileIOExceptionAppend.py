02266ff219e33bec3e2e34a0f58cb53668517f42
Tanishq chandole <tanishqchandole@gmail.com>
2026-03-27 12:16:11 +0530

    Codes

---
+++ Codes/FileIOExceptionAppend.py
@@ -0,0 +1,18 @@
+def main():
+    try:
+        fobj = open("Hello.txt","a")
+        print("File gets successfully opened")
+
+        fobj.write("Python Automation")
+
+        fobj.close()
+
+    except FileNotFoundError:
+        print("Unable to open file as their is no such file")
+
+    finally:
+        print("End of application") 
+            
+
+if __name__ == "__main__":
+    main()
